9f592f84114b33d287249042cf3ec8d4f848cac2
SkobelkinVitaly <>
2019-05-05 20:28:37 +0300

    Лабораторная №3 выполнена частично. Студент - Скобелкин Виталий Сергеевич.

---
+++ Lab3/src/ru/java/avalon/Application.java
@@ -0,0 +1,14 @@
+package ru.java.avalon;
+
+import ru.avalon.java.frames.*;
+import javax.swing.*;
+
+public class Application {
+
+
+    public static void main(String[] args) {
+        JFrame frame = new ColorPicker();
+        frame.setVisible(true);
+    }
+    
+}
